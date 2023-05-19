700673f79ea143ccddab3fee9582d2de6317bcd4
Valeria Romero <valellope@gmail.com>
2023-05-19 00:28:14 -0600

    POST PUT DELETE GET Listo

--- src/main/java/com/example/valeria/services/UsuarioService.java
+++ src/main/java/com/example/valeria/services/UsuarioService.java
@@ -1,6 +1,7 @@
 package com.example.valeria.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,8 +18,26 @@ public class UsuarioService {
 	public List<UsuarioModel> obtenerUsuarios(){
 		return (List<UsuarioModel>) usuarioRepository.findAll();
 	}
+
 	
 	public UsuarioModel guadarUsuario(UsuarioModel usuario) {
 		return usuarioRepository.save(usuario);
+	}
+	
+	public Optional<UsuarioModel> obtenerPorId(Long id){
+		return usuarioRepository.findById(id);
+	}
+	
+	public List<UsuarioModel> obtenerPorPrioridad(Integer prioridad){
+		return usuarioRepository.findByPrioridad(prioridad);
+	}
+	
+	public boolean eliminarUsuario(Long id) {
+		try {
+			usuarioRepository.deleteById(id);
+			return true;
+		} catch(Exception err) {
+			return false;
+		}
 	}
 }
